34a0240d4c7aac67464404672c5d0a4bde7040ed
5ZYSZ3K <paw.por@wp.pl>
2022-08-27 20:07:04 +0200

    Web and native started to work

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {NativeNavigation} from '@app/navigation/native';
 import {Provider} from '@app/provider';
+import 'react-native-gesture-handler';
 
 const App: React.FC = () => {
   return (
